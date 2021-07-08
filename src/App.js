d643f13d8c8bc4169445139ddd6c4c1e216a6b65
AirMaks <rm1010@yandex.ru>
2021-07-08 11:33:47 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+
+function computeInitialState() {
+  console.log('Some manipulations...');
+  return Math.trunc(Math.random() * 10)
+}
 
 function App() {
+
+  const [counter, setCounter] = useState(() => {
+    return computeInitialState()
+  });
+
+  const [state, setState] = useState({
+    title: 'Title',
+    number: 21
+  })
+
+  const increment = () => {
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+    setCounter(prevCounter => prevCounter + 1)
+  }
+
+  const decrement = () => {
+    setCounter(counter - 1)
+  }
+
+  const changeTitle = () => {
+    setState(prev => {
+      return {
+        ...prev,
+        title: 'New title'
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Счетчик: {counter}</h1>
+      <button onClick={increment}>Add</button>
+      <button onClick={decrement}>Remove</button> 
+      <button onClick={changeTitle}>Change title</button>
+
+      <pre>{JSON.stringify(state, null, 2)}</pre> 
     </div>
   );
 }
