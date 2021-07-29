9c37c9da29ee2b7f740d3d52035ceea619c69612
AirMaks <rm1010@yandex.ru>
2021-07-29 13:27:21 +0300

    useState calculator

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
+
 
 
 function App() {
 
+  let [num1, setNum1] = useState(0);
+  let [num2, setNum2] = useState(0);
+  const [total, setTotal] = useState(num1 + num2);
+
+  function changeNum1(e) {
+    console.log('some...');
+    var newNum1 = +(e.target.value)
+    setNum1(num1 = newNum1)
+    setTotal(num2 + newNum1)
+  }
+
+  function changeNum2(e) {
+    var newNum2 = +(e.target.value)
+    setNum2(num2 = newNum2)
+    setTotal(num1 + newNum2)
+  }
 
   return (
-    <div className="App">
-   
+    <div>
+      <input value={num1} type="text" onChange={changeNum1} />
+      <span> + </span>  
+      <input value={num2} type="text" onChange={changeNum2}  />
+      <span> = </span>
+      <span>{total}</span>
     </div>
-  );
+  )
 }
 
 export default App;
+
+
+
