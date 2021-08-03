d15666edec9e80f8233fd4ee4868e97917d8c442
AirMaks <rm1010@yandex.ru>
2021-08-03 12:55:50 +0300

    own hooks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import Alert from './alert/Alert.js';
-import { AlertProvider } from './alert/AlertContext.js';
-import Main from './Main.js';
+import React, {useEffect, useState} from 'react';
+
+function useLogger(value) {
+    useEffect(() => {
+        console.log('value changed');
+    }, [value])
+}
+
+
+function useInput(initialValue) {
+    const [value, setValue] = useState(initialValue);
+
+    const onChange = e => {
+        setValue(e.target.value);
+    }
+
+    const clear = () => setValue('');
+
+    return {
+        bind: {value, onChange},
+        value, clear
+    }
+
+}
+
 
 
 function App() {
+
+    const input = useInput('');
+
+    useLogger(input.value);
+
     return (
-        <AlertProvider>
-            <div>
-                <Main toggle={() => {}}/>
-                <Alert />
-            </div>  
-        </AlertProvider>
+        <div>
+            <input type="text" {...input.bind}/>
+            <button onClick={() => input.clear()}>Clear</button>
+            <h1>{input.value}</h1>
+        </div>
     )
+   
 }
 
 export default App;
